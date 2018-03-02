296a5398d91b31be2d8094fb2139d3740835ae44
Alan Eduardo <alan_eduardo@outlook.com.br>
2018-03-02 12:05:27 -0300

    Implementing the MainNav component in the App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Nav, NavItem, NavLink } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 
-import logo from '../../assets/images/logo-head.png';
+import MainNav from '../MainNav/MainNav';
+
 import './App.scss';
 
 class App extends Component {
@@ -9,33 +10,8 @@ class App extends Component {
     return (
       <Container className="App">
         <Row>
-          <Col md="3" className="main-nav">
-            <p className="title">
-              <img src={logo} height="80" /><br />
-              Alan Eduardo Software Artisan
-            </p>
-            <Nav vertical>
-              <NavItem>
-                <NavLink href="#">Personal</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#">Professional</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#">Companies</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink disabled href="#">Playground</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink disabled href="#">Contact</NavLink>
-              </NavItem>
-            </Nav>
-            <hr />
-            <p>Link based</p>
-            <Nav vertical>
-              <NavLink href="#">Link</NavLink> <NavLink href="#">Link</NavLink> <NavLink href="#">Another Link</NavLink> <NavLink disabled href="#">Disabled Link</NavLink>
-            </Nav>
+          <Col md="3">
+            <MainNav />
           </Col>
           <Col md="9"/>
         </Row>
