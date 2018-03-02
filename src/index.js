479e08531cd28a7f05a3034d37ba4c9b620a7460
Alan Eduardo <alan_eduardo@outlook.com.br>
2018-03-02 10:40:05 -0300

    Defining the final version for the boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+
+import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
