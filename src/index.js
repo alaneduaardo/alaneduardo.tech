453ced22ba346962de489c91565f331cdcaa9ed0
Alan Eduardo <alan_eduardo@outlook.com.br>
2018-03-19 19:55:33 -0300

    Configuring the React Router in the app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -7,5 +8,11 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/example" component={App} />
+    </Switch>
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
